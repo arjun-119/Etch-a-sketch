7c539ce6219a6490f26636430d3bcc4049147132
AB <arjunkrish2003@gmail.com>
2024-05-18 17:46:45 +0530

    Make a 16 by 16 grid wihout the css properties.

--- js-file.js
+++ js-file.js
@@ -1,16 +1,18 @@
 let containerDiv = document.querySelector(".container");
 let btn = document.querySelector("#gridSize");
 
-function createRows(columns){
-   let rowDiv = document.createElement("div");
-    rowDiv.classList.add("row");
+function createRows(){
+    for( let i =0;i<17;i++){
+        let rowDiv = document.createElement("div");
+         rowDiv.classList.add("row");
 
-    for( let i =0; i<columns;i++){
-        columnDiv = document.createElement("div");
-        columnDiv.classList.add("column");
-        columnDiv.textContent= i;
+         columnDiv = document.createElement("div");
+         columnDiv.classList.add("column");
+         columnDiv.textContent= i;
         
-        rowDiv.appendChild(columnDiv);
-}
+            rowDiv.appendChild(columnDiv);
+
     containerDiv.appendChild(rowDiv);
+    }
 }
+createRows();
